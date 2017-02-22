57db6c055e2c0c0009133bb135c054deab46d1b4
mfpebrizal <ftm.pebrizal@gmail.com>
2017-02-22 22:27:50 +0700

    Done

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
+const colorPalette = require('./palette');
+const getPallete = require('./pallete');
 
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-  res.render("index", { palette: ["#69D2E7", "#E0E4CC", "#F38630" });
+  res.render("index", { palette: getPallete()});
 });
 
 console.log("Server runing on http://localhost:9000")
